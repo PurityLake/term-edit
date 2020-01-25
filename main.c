9e89ecc71652e3ad1eac330f9307731bb302cf3f
Robert O'Shea <robertoshea2k11@gmail.com>
2020-01-25 17:37:22 +0000

    Added colour to borders and line numbers

--- main.c
+++ main.c
@@ -3,27 +3,47 @@
 
 #include "line.h"
 
+#define BORDER_PAIR 1
+#define LINE_NO_PAIR 2
+
 void print_border(int row, int col);
 void print_screen(int row, int col, int x, int y, line_list *list);
+
+int min(int a, int b) { 
+    return (a < b) ? a : b;
+}
 
 int main(int argc, char **argv) {
     line_list *list = create_line_list();
     add_to_end_of_list(list, create_line(1, 0, 32));
     line_list *curr = list;
 
-    char ch;
+    int ch;
 
     initscr();
     raw();
     keypad(stdscr, TRUE);
-    noecho();			 
+    noecho();
+
+    if (has_colors()) {
+        start_color();
+        init_pair(BORDER_PAIR, COLOR_CYAN, COLOR_BLACK);
+        init_pair(LINE_NO_PAIR, COLOR_YELLOW, COLOR_BLACK);
+    } else {
+        endwin();
+        free_line_list(list);
+        return EXIT_FAILURE;
+    } 
 
     int row, col;
     getmaxyx(stdscr, row, col);
 
     print_border(row, col);
 
+    attron(COLOR_PAIR(LINE_NO_PAIR));
     printw("1. ");
+    attroff(COLOR_PAIR(LINE_NO_PAIR));
+
     int x, y;
     getyx(stdscr, y, x);
     //int curLine = 1;
@@ -32,12 +52,22 @@ int main(int argc, char **argv) {
         if (ch == '\n') {
             ++y;
             move(y, 1);
+            attron(COLOR_PAIR(LINE_NO_PAIR));
             printw("%d. ", y);
+            attroff(COLOR_PAIR(LINE_NO_PAIR));
             add_to_end_of_list(list, create_line(y, 0, 32));
             curr = curr->next;
             print_screen(row, col, x, y, list);
+        } else if (ch == KEY_UP) {
+            if (y > 1) {
+                --y;
+                curr = curr->prev;
+                x = strlen(curr->value->value) + 4;
+                move(y, x);
+            }
         } else {
-            strncat(curr->value->value, &ch, 1);
+            char c = (char)ch;
+            strncat(curr->value->value, &c, 1);
             printw("%c", ch);
         }
     }
@@ -46,10 +76,11 @@ int main(int argc, char **argv) {
     
     free_line_list(list);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 void print_border(int row, int col) {
+    attron(COLOR_PAIR(BORDER_PAIR));
     for (int i = 0; i < col; ++i) {
         mvprintw(0, i, "-");
         mvprintw(row-1, i, "-");
@@ -62,6 +93,7 @@ void print_border(int row, int col) {
     mvprintw(0, col-1, "+");
     mvprintw(row-1, 0, "+");
     mvprintw(row-1, col-1, "+");
+    attroff(COLOR_PAIR(BORDER_PAIR));
     move(1, 1);
 }
 
@@ -70,10 +102,16 @@ void print_screen(int row, int col, int x, int y, line_list *list) {
     print_border(row, col);
     line_list *curr = list;
     while (curr->next != NULL) {
-        printw("%d. %s", curr->value->lineNo, curr->value->value);
+        attron(COLOR_PAIR(LINE_NO_PAIR));
+        printw("%d. ", curr->value->lineNo);
+        attroff(COLOR_PAIR(LINE_NO_PAIR));
+        printw("%s", curr->value->value);
         move(curr->value->lineNo + 1, 1);
         curr = curr->next;
     }
-    printw("%d. %s", curr->value->lineNo, curr->value->value);
+    attron(COLOR_PAIR(LINE_NO_PAIR));
+    printw("%d. ", curr->value->lineNo);
+    attroff(COLOR_PAIR(LINE_NO_PAIR));
+    printw("%s", curr->value->value);
     move(y, x);
 }
